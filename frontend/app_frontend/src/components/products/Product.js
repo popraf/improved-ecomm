d60a74d2b0fcbef8787bf5e2cacc665060d0f159
Rafał <rafal44334@gmail.com>
2023-02-25 10:54:16 +0100

    Debugging of landing page - a simple API/latest prod works

--- frontend/app_frontend/src/components/products/Product.js
+++ frontend/app_frontend/src/components/products/Product.js
@@ -5,15 +5,35 @@ import { Link } from "react-router-dom";
 function Product({ product }) {
     return (
         <Card className="my-3 p-3 rounded">
-
-            <a Link to={'/product/${product._id}'}>
-                <div>
-                    <h5>we are in productjs</h5>
-                    {product.rating} from {product.numReviews} reviews
-
-                    <ProductRating />
-                </div>
+            <a href={`/product/${product._id}`}>
+                <Card.Img src={product.image}/>
             </a>
+
+            <Card.Body>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as="div">
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+
+                <Card.Text as="div">
+                    <div className="my-3">
+                        {product.rating} from {product.numReviews} reviews
+                    </div>
+                </Card.Text>
+
+{/* 
+                <a Link to={'/product/${product._id}'}>
+                    <div>
+                        <h5>we are in productjs</h5>
+                        {product.rating} from {product.numReviews} reviews
+
+                        <ProductRating />
+                    </div>
+                </a> */}
+
+            </Card.Body>
+
 
         </Card>
     )
--- frontend/app_frontend/src/components/screens/LandingPage.js
+++ frontend/app_frontend/src/components/screens/LandingPage.js
@@ -23,8 +23,8 @@ function LandingPage() {
             <Row>
                 {productsz.map(
                     product => (
-                        <Col sm={12} md={6} lg={4} xl={3}>
-                            <h3>{product.name}</h3>
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            < Product product={product} />
                         </Col>
                     )
                 )}
